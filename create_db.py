7c97d3bf6c3d76d654d04bbd7a318ba7ea8beb39
Andrii Kurinnyi <andrew@marpasoft.com>
2010-12-31 00:14:36 -0800

    Adjusted create_db and shell scripts

--- create_db.py
+++ create_db.py
@@ -1,2 +1,3 @@
-from application import db
+#!/usr/bin/env python
+from route53.models import db
 db.create_all()
--- shell.py
+++ shell.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env ipython
-from application import *
+from route53 import app
 
 ctx = app.test_request_context()
 ctx.push()
