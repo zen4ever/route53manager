441f787d90219d6a86ad6d7e27239c51eed22f95
Andrii Kurinnyi <andrew@marpasoft.com>
2011-01-02 01:07:07 -0800

    Added basic styling with sass and blueprint

---
+++ sass/config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+http_images_path = "/static/img/"
+css_dir = "../route53/static/css"
+sass_dir = "src"
+images_dir = "../route53/static/img"
+javascripts_dir = "javascripts"
+output_style = :compact
+line_comments = false
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+preferred_syntax = :sass
