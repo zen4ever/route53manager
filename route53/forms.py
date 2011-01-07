3e0f473c3f880e14977ed5fb2b4b7db827966945
Andrii Kurinnyi <andrew@marpasoft.com>
2011-01-07 03:11:51 -0800

    Removed old AWSCredentials form, added comment field to record form

--- route53/forms.py
+++ route53/forms.py
@@ -15,14 +15,6 @@ RECORD_CHOICES = [
 ]
 
 
-class AWSCredentialForm(wtf.Form):
-    nickname = wtf.TextField('Nickname', validators=[validators.Required()])
-    access_key_id = wtf.TextField('Access Key ID',
-            validators=[validators.Required()])
-    secret_access_key = wtf.TextField('Secret Access Key',
-            validators=[validators.Required()])
-
-
 class ZoneForm(wtf.Form):
     name = wtf.TextField('Domain Name', validators=[validators.Required()])
     comment = wtf.TextAreaField('Comment')
@@ -34,3 +26,4 @@ class RecordForm(wtf.Form):
     value = wtf.TextField("Value", validators=[validators.Required()])
     ttl = wtf.IntegerField("TTL", default="86400",
             validators=[validators.Required()])
+    comment = wtf.TextAreaField("Comment")
