af90eb62275524f0eabb7bb06df325cb502776e0
Andrii Kurinnyi <andrew@marpasoft.com>
2011-01-07 03:12:16 -0800

    Added comment field to batch model

--- route53/models.py
+++ route53/models.py
@@ -14,6 +14,7 @@ class ChangeBatch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     change_id = db.Column(db.String(255))
     status = db.Column(db.String(255))
+    comment = db.Column(db.String(255))
 
     changes = db.relation("Change", backref="change_batch")
 
