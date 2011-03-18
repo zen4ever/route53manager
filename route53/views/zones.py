3cf5bff21eb4734b71215fb248fe084b5caf93ba
Andrii Kurinnyi <andrew@marpasoft.com>
2011-03-17 22:00:57 -0700

    Added ability to clone zone with all DNS rules but specify new domain name, fixes #3

--- route53/views/zones.py
+++ route53/views/zones.py
@@ -5,8 +5,9 @@ from flask import url_for, render_template, \
         redirect, flash, request
 
 from route53.forms import ZoneForm
-from route53.xmltools import etree
 from route53.connection import get_connection
+
+from route53.xmltools import render_change_batch
 
 zones = Module(__name__)
 
@@ -85,10 +86,75 @@ def zones_records(zone_id):
 
     record_resp = conn.get_all_rrsets(zone_id)
 
-    from route53.xmltools import RECORDSET_TAG, NAME_TAG, TYPE_TAG, \
-            TTL_TAG, RECORDS_TAG, RECORD_TAG, VALUE_TAG
-
     return render_template('zones/records.html',
             zone_id=zone_id,
             zone=zone,
             recordsets=record_resp)
+
+
+@zones.route('/clone/<zone_id>', methods=['GET', 'POST'])
+def zones_clone(zone_id):
+    conn = get_connection()
+
+    zone_response = conn.get_hosted_zone(zone_id)
+    original_zone = zone_response['GetHostedZoneResponse']['HostedZone']
+
+    form = ZoneForm()
+    errors = []
+
+    if form.validate_on_submit():
+        response = conn.create_hosted_zone(
+                form.name.data,
+                comment=form.comment.data)
+
+        info = response['CreateHostedZoneResponse']
+
+        nameservers = ', '.join(info['DelegationSet']['NameServers'])
+
+        new_zone_id = info['HostedZone']['Id']
+
+        original_records = conn.get_all_rrsets(zone_id)
+
+        from route53.models import ChangeBatch, Change, db
+
+        for recordset in original_records:
+            if not recordset.type in ["SOA", "NS"]:
+
+                print recordset.type
+
+                change_batch = ChangeBatch(change_id='',
+                                           status='created',
+                                           comment='')
+                db.session.add(change_batch)
+                change = Change(action="CREATE",
+                                name=recordset.name.replace(original_zone['Name'],
+                                                            form.name.data),
+                                type=recordset.type,
+                                ttl=recordset.ttl,
+                                values = recordset.resource_records,
+                                change_batch_id=change_batch.id)
+
+                db.session.add(change)
+                changes = [change]
+
+                rendered_xml = render_change_batch({'changes': changes, 'comment': ''})
+
+                print rendered_xml
+
+                try:
+                    from route53 import shortid
+                    resp = conn.change_rrsets(shortid(new_zone_id), rendered_xml)
+                    change_batch.process_response(resp)
+                    db.session.commit()
+                except DNSServerError as error:
+                    errors.append((recordset.type, recordset.name, error))
+                    db.session.rollback()
+
+        if not errors:
+            flash(u"A zone with id %s has been created. "
+                  u"Use following nameservers %s"
+                   % (new_zone_id, nameservers))
+            return redirect(url_for('zones_list'))
+
+    return render_template('zones/clone.html',
+        form=form, errors=errors, original_zone=original_zone)
